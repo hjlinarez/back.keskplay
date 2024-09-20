4ae32e47ff2d7d29181c515a2f45efaf12421577
Hector Linarez <hjlinarez@gmail.com>
2024-09-20 17:23:49 -0400

    Otro intento

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,7 +2,7 @@
    
     <div class="nav">
         <div class="sb-sidenav-menu-heading">Operador</div>
-        <a class="nav-link" href="home">
+        <a class="nav-link" href="\">
             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
             Inicio
         </a>
