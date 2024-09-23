026ff0910d43cfaf1aaa4c7681bd2ec7c5f9a7ed
Hector Linarez <hjlinarez@gmail.com>
2024-09-23 18:08:18 -0400

    Update del dia

--- app/Http/Livewire/CajaController.php
+++ app/Http/Livewire/CajaController.php
@@ -1,46 +1,38 @@
 <?php
 
 namespace App\Http\Livewire;
-
 use Livewire\Component;
 use App\Models\Cajas;
+use PhpParser\Node\Stmt\TryCatch;
+use Spatie\FlareClient\Context\RequestContextProvider;
 
 class CajaController extends Component
 {   
     public $caja;
-    public $name;
-    public $email;
-    public $login;
-    public $password;
-    public $repeat_password;
+    protected $rules = [
+                        'caja.name'=> 'required',
+                        'caja.email'=> 'required',
+                        'caja.login'=> 'required',
+                        'caja.apuesta_minima'=> 'required',
+                        'caja.apuesta_maxima'=> 'required',
+                        'caja.pagos'=> 'required',
 
+                        'caja.acumulado_mini_jackpot'=> 'required',
+                        'caja.porc_mini_jackpot'=> 'required',
+                        'caja.limite_mini_jackpot'=> 'required',
+                        'caja.porc_entrega_mini_jackpot'=> 'required',
 
-    public $apuesta_minima;
-    public $apuesta_maxima;
-    public $pagos;
+                        'caja.acumulado_super_jackpot'=> 'required',
+                        'caja.porc_super_jackpot'=> 'required',
+                        'caja.limite_super_jackpot'=> 'required',
+                        'caja.porc_entrega_super_jackpot'=> 'required',
 
-    //VARIABLES MINI JACKPOT
-    public $acumulado_mini_jackpot;
-    public $porc_mini_jackpot;
-    public $limite_mini_jackpot;
-    public $porc_entrega_mini_jackpot;
+                        'caja.acumulado_mega_jackpot'=> 'required',
+                        'caja.porc_mega_jackpot'=> 'required',
+                        'caja.limite_mega_jackpot'=> 'required',
+                        'caja.porc_entrega_mega_jackpot'=> 'required'
+                    ];
 
-
-    //VARIABLES SUPER JACKPOT
-    public $acumulado_super_jackpot;
-    public $porc_super_jackpot;
-    public $limite_super_jackpot;
-    public $porc_entrega_super_jackpot;
-
-
-    //VARIABLES MEGA JACKPOT
-    public $acumulado_mega_jackpot;
-    public $porc_mega_jackpot;
-    public $limite_mega_jackpot;
-    public $porc_entrega_mega_jackpot;
-
-
-    public $mensaje;
 
 
 
@@ -51,7 +43,6 @@ class CajaController extends Component
     {
         $this->filtro = '';
         $this->estatus = 'ALL';
-        
 
     }
 
@@ -74,7 +65,133 @@ class CajaController extends Component
 
     public function editcaja(Cajas $caja)
     {
-        $this->caja = $caja;
-        $this->emit('EditCaja'); 
+        $this->skipRender();
+        $this->caja = $caja;        
+        $this->emit('open_modal');         
+    }
+
+    public function GuardarCaja()
+    {
+
+        if ($this->caja->apuesta_minima <= 0)
+        {            
+            $this->emit('mensaje', "Disculpe", "Apuesta minimaaaa es invalida", "error");    
+        } 
+        
+        
+        
+        if ($this->caja->name == null){ $this->emit('mensaje', "Disculpe", "Debe indicar el nombre de la caja", "error"); return false;} 
+        if ($this->caja->email == null){ $this->emit('mensaje', "Disculpe", "Indique el correo electronico", "error"); return false;} 
+        if ($this->caja->login == null){ $this->emit('mensaje', "Disculpe", "Indique el login de la caja", "error"); return false;} 
+        
+
+
+        $cantidad_opciones = count(explode(",",$this->caja->pagos));
+        if ($cantidad_opciones <= 1) { $this->emit('mensaje', "Disculpe", "Campo 'Opciones de Pago' es invalido ".$cantidad_opciones, "error"); return false;} 
+
+        
+
+        
+
+        if ($this->caja->apuesta_maxima <= 0)
+        {
+            $this->emit('mensaje', "Disculpe", "Apuesta Maxima es invalida", "error"); 
+            return false;
+        }
+
+        if ($this->caja->apuesta_maxima <= $this->caja->apuesta_minima)
+        {
+            $this->emit('mensaje', "Disculpe", "Apuesta maxima deber ser mayor que la apuesta minima", "error"); 
+            return false;
+        }
+
+        //validacion del MINI jakcpo
+        if ($this->caja->acumulado_mini_jackpot < 0)
+        {
+            $this->emit('mensaje', "Disculpe", "El monto inicial del mini jackpot es invalido", "error"); 
+            return false;
+        }
+
+        if ($this->caja->porc_mini_jackpot < 0 or $this->caja->porc_mini_jackpot >= 5)
+        {
+            $this->emit('mensaje', "Disculpe", "El porcentaje de recoleccion del mini jackpot maximo debe ser el 5%", "error"); 
+            return false;
+        }
+
+        if ($this->caja->limite_mini_jackpot < 0)
+        {
+            $this->emit('mensaje', "Disculpe", "El monto limite de entrega del mini jackpot es invalido", "error"); 
+            return false;
+        }
+
+        if ($this->caja->porc_entrega_mini_jackpot < 0 or $this->caja->porc_entrega_mini_jackpot >= 100)
+        {
+            $this->emit('mensaje', "Disculpe", "El porcentaje de entrega del mini jackpot es invalido", "error"); 
+            return false;
+        }
+
+        //validacion del SUPER jakcpo
+        if ($this->caja->acumulado_super_jackpot < 0)
+        {
+            $this->emit('mensaje', "Disculpe", "El monto inicial del super jackpot es invalido", "error"); 
+            return false;
+        }
+
+        if ($this->caja->porc_super_jackpot < 0 or $this->caja->porc_super_jackpot >= 5)
+        {
+            $this->emit('mensaje', "Disculpe", "El porcentaje de recoleccion de super jackpot maximo debe ser el 5%", "error"); 
+            return false;
+        }
+
+        if ($this->caja->limite_super_jackpot < 0)
+        {
+            $this->emit('mensaje', "Disculpe", "El monto limite de entrega super jackpot es invalido", "error"); 
+            return false;
+        }
+
+        if ($this->caja->porc_entrega_super_jackpot < 0 or $this->caja->porc_entrega_super_jackpot >= 100)
+        {
+            $this->emit('mensaje', "Disculpe", "El porcentaje de entrega del super jackpot es invalido", "error"); 
+            return false;
+        }
+
+        //validacion del MEGA jakcpo
+        if ($this->caja->acumulado_mega_jackpot < 0)
+        {
+            $this->emit('mensaje', "Disculpe", "El monto inicial del mega jackpot es invalido", "error"); 
+            return false;
+        }
+
+        if ($this->caja->porc_mega_jackpot < 0 or $this->caja->porc_mega_jackpot >= 5)
+        {
+            $this->emit('mensaje', "Disculpe", "El porcentaje de recoleccion del mega jackpot maximo debe ser el 5%", "error"); 
+            return false;
+        }
+
+        if ($this->caja->limite_mega_jackpot < 0)
+        {
+            $this->emit('mensaje', "Disculpe", "El monto limite de entrega del super jackpot es invalido", "error"); 
+            return false;
+        }
+
+        if ($this->caja->porc_entrega_mega_jackpot < 0 or $this->caja->porc_entrega_mega_jackpot >= 100)
+        {
+            $this->emit('mensaje', "Disculpe", "El porcentaje de entrega de mega jackpot es invalido", "error"); 
+            return false;
+        }
+
+        try {
+            if ($this->caja->save())
+            {
+                $this->emit('close_modal');
+                $this->emit('mensaje', "Exito", "Registro actualizado","success"); 
+                
+            }
+        } catch (\Exception $e) {
+            
+            $this->emit('mensaje', "Disculpe", $e->getMessage(), "error"); 
+        }
+
+
     }
 }
--- resources/views/livewire/caja-controller.blade.php
+++ resources/views/livewire/caja-controller.blade.php
@@ -40,53 +40,39 @@
 
                
             @endforeach
+       
 
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
 
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                Launch static backdrop modal
-              </button>
-
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-dialog modal-lg modal-dialog-centered">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="staticBackdropLabel">Modificar Caja - {{ $caja ? $caja->name : ''}}</h1>
+                      <h1 class="modal-title fs-5" id="staticBackdropLabel">Modificar Caja</h1>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-
-
-
 
 
                         <form action="">
                             <div class="row">
                                 <div class="col-md-6">
                                     <label for="name" class="form-label">Nombre</label>
-                                    <input type="text" class="form-control" id="name" required wire:model="caja.name" placeholder="Indique el nombre de la Caja"/>
+                                    <input type="text" class="form-control" id="name"  wire:model.defer="caja.name"  placeholder="Indique el nombre de la Caja" />
                                 </div>
                     
                                 <div class="col-md-6">
                                     <label for="name" class="form-label">Correo Electronico</label>
-                                    <input type="email" class="form-control" id="email" required wire:model="email" placeholder="abc@mail.com"/>
+                                    <input type="email" class="form-control" id="email"  wire:model.defer="caja.email" placeholder="abc@mail.com"/>
                                 </div>
                             </div>
                     
                             <div class="row">
                                 <div class="col-md-4">
                                     <label for="name" class="form-label">Usuario</label>
-                                    <input type="text" class="form-control" id="login" required wire:model="login"/>
+                                    <input type="text" class="form-control" id="login"  disabled wire:model.defer="caja.login" />
                                 </div>
                     
-                                <div class="col-md-4">
-                                    <label for="name" class="form-label">Clave</label>
-                                    <input type="password" class="form-control" id="password" required wire:model="password"/>
-                                </div>
-                    
-                                <div class="col-md-4">
-                                    <label for="name" class="form-label">Repita la Clave</label>
-                                    <input type="password" class="form-control" id="repeat_password" required wire:model="repeat_password"/>
-                                </div>
+                                
                             </div>
                             <hr>
                             <h5>Config Keno</h5>
@@ -94,17 +80,17 @@
                             <div class="row">
                                 <div class="col">
                                     <label for="apuesta_minima" class="form-label">Apuesta Minima</label>
-                                    <input type="number" class="form-control" id="apuesta_minima" wire:model="apuesta_minima" placeholder=""/>
+                                    <input type="number" class="form-control" id="apuesta_minima" wire:model.defer="caja.apuesta_minima" placeholder=""/>
                                 </div>
                     
                                 <div class="col">
                                     <label for="apuesta_maxima" class="form-label">Apuesta Maxima</label>
-                                    <input type="number" class="form-control" id="apuesta_maxima" wire:model="apuesta_maxima"/>
+                                    <input type="number" class="form-control" id="apuesta_maxima" wire:model.defer="caja.apuesta_maxima"/>
                                 </div>
                     
                                 <div class="col">
                                     <label for="apuesta_maxima" class="form-label">Opciones de Pago</label>
-                                    <input type="text" class="form-control" id="pagos" wire:model="pagos" placeholder="Ej: 100,200,300"/>
+                                    <input type="text" class="form-control" id="pagos" wire:model.defer="caja.pagos" placeholder="Ej: 100,200,300"/>
                                 </div>
                     
                             </div>
@@ -120,16 +106,16 @@
                                         <div class="card-body">
                                             
                                             <label for="acumulado_mini_jackpot " class="form-label">Monto Inicial</label>
-                                            <input type="number" class="form-control" id="acumulado_mini_jackpot" wire:model="acumulado_mini_jackpot"/>
+                                            <input type="number" class="form-control" id="acumulado_mini_jackpot" wire:model.defer="caja.acumulado_mini_jackpot"/>
                     
                                             <label for="porc_mini_jackpot" class="form-label">% Recoleccion</label>
-                                            <input type="number" class="form-control" id="porc_mini_jackpot" wire:model="porc_mini_jackpot"/>
+                                            <input type="number" class="form-control" id="porc_mini_jackpot" wire:model.defer="caja.porc_mini_jackpot"/>
                     
                                             <label for="limite_mini_jackpot" class="form-label">Lim. Entrega</label>
-                                            <input type="number" class="form-control" id="limite_mini_jackpot" wire:model="limite_mini_jackpot"/>
+                                            <input type="number" class="form-control" id="limite_mini_jackpot" wire:model.defer="caja.limite_mini_jackpot"/>
                     
                                             <label for="porc_entrega_mini_jackpot" class="form-label">% Entrega</label>
-                                            <input type="number" class="form-control" id="porc_entrega_mini_jackpot" wire:model="porc_entrega_mini_jackpot"/>
+                                            <input type="number" class="form-control" id="porc_entrega_mini_jackpot" wire:model.defer="caja.porc_entrega_mini_jackpot"/>
                                             
                                         </div>
                                     </div>
@@ -140,16 +126,16 @@
                                         <div class="card-header">Super</div>
                                         <div class="card-body">
                                             <label for="acumulado_super_jackpot " class="form-label">Monto Inicial</label>
-                                            <input type="number" class="form-control" id="acumulado_super_jackpot" wire:model="acumulado_super_jackpot"/>
+                                            <input type="number" class="form-control" id="acumulado_super_jackpot" wire:model.defer="caja.acumulado_super_jackpot"/>
                     
                                             <label for="porc_super_jackpot" class="form-label">% Recoleccion</label>
-                                            <input type="number" class="form-control" id="porc_super_jackpot" wire:model="porc_super_jackpot" />
+                                            <input type="number" class="form-control" id="porc_super_jackpot" wire:model.defer="caja.porc_super_jackpot" />
                     
                                             <label for="limite_super_jackpot" class="form-label">Lim. Entrega</label>
-                                            <input type="number" class="form-control" id="limite_super_jackpot" wire:model="limite_super_jackpot" />
+                                            <input type="number" class="form-control" id="limite_super_jackpot" wire:model.defer="caja.limite_super_jackpot" />
                     
                                             <label for="porc_entrega_super_jackpot" class="form-label">% Entrega</label>
-                                            <input type="number" class="form-control" id="porc_entrega_super_jackpot" wire:model="porc_entrega_super_jackpot" />
+                                            <input type="number" class="form-control" id="porc_entrega_super_jackpot" wire:model.defer="caja.porc_entrega_super_jackpot" />
                                         </div>
                                     </div>
                                 </div>
@@ -159,16 +145,16 @@
                                         <div class="card-header">Mega</div>
                                         <div class="card-body">
                                             <label for="acumulado_mega_jackpot " class="form-label">Monto Inicial</label>
-                                            <input type="number" class="form-control" id="acumulado_mega_jackpot" wire:model="acumulado_mega_jackpot"/>
+                                            <input type="number" class="form-control" id="acumulado_mega_jackpot" wire:model.defer="caja.acumulado_mega_jackpot"/>
                     
                                             <label for="porc_mega_jackpot" class="form-label">% Recoleccion</label>
-                                            <input type="number" class="form-control" id="porc_mega_jackpot" wire:model="porc_mega_jackpot"/>
+                                            <input type="number" class="form-control" id="porc_mega_jackpot" wire:model.defer="caja.porc_mega_jackpot"/>
                     
                                             <label for="limite_mega_jackpot" class="form-label">Lim. Entrega</label>
-                                            <input type="number" class="form-control" id="limite_mega_jackpot" wire:model="limite_mega_jackpot"/>
+                                            <input type="number" class="form-control" id="limite_mega_jackpot" wire:model.defer="caja.limite_mega_jackpot"/>
                     
                                             <label for="porc_entrega_mega_jackpot" class="form-label">% Entrega</label>
-                                            <input type="number" class="form-control" id="porc_entrega_mega_jackpot" wire:model="porc_entrega_mega_jackpot"/>
+                                            <input type="number" class="form-control" id="porc_entrega_mega_jackpot" wire:model.defer="caja.porc_entrega_mega_jackpot"/>
                                         </div>
                                     </div>
                                 </div>
@@ -183,7 +169,7 @@
                     </div>
                     <div class="modal-footer">
                       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-primary">Modificar</button>
+                      <button  type="button" class="btn btn-primary" wire:click.prevent=GuardarCaja>Modificar</button>
                     </div>
                   </div>
                 </div>
@@ -194,7 +180,28 @@
 <script>
 
     document.addEventListener('livewire:load', function () {
-        Livewire.on('EditCaja', () => {  $('#staticBackdrop').modal('toggle'); });
+
+
+        Livewire.on('open_modal', () => {  
+
+                $('#staticBackdrop').modal('show');
+
+            });
+
+
+        Livewire.on('close_modal', () => {  
+                $('#staticBackdrop').modal('toggle'); 
+            });
+
+
+        Livewire.on('mensaje', (titulo, mensaje, tipo) => { 
+                swal(titulo, mensaje, tipo);
+            } );
+
+        
+
+                       
+        
     });
 
 </script>
